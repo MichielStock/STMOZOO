21670b8538b6a04576234983dcdac345c776951a
michielstock <michielfmstock@gmail.com>
2020-10-30 17:25:57 +0100

    deployment docs

--- docs/make.jl
+++ docs/make.jl
@@ -5,3 +5,7 @@ using STMOZOO
 makedocs(sitename="STMO ZOO",
     modules=[Example] # add your module
         )
+
+deploydocs(
+            repo = "github.com/michielstock/STMOZOO.jl.git",
+        )
