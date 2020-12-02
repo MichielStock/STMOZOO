bcb57439a194166334e63b5a1665ef449422f788
michielstock <michielfmstock@gmail.com>
2020-12-02 09:24:55 +0100

    :tada: softmax code

--- src/softmax.jl
+++ src/softmax.jl
@@ -9,8 +9,18 @@ module Softmax
 # if you use a new package, don't forget to add it in the package manager
 
 # export all functions that are relevant for the user
-#export 
+export softmax
 
+function softmax(x::Vector; κ::Number=1.0)
+	q = exp.(x)
+	q ./= sum(q)
+	return q
+end
 
+function gumbel_max(items::Vector, x::Vector; κ::Number=1.0)
+    @assert length(items) == length(x) "length of `items` and `x` do not match"
+    i = κ .* x .+ rand(Gumbel(), length(x)) |> argmax
+    return items[i]
+end
 
 end
