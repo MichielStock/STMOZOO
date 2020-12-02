a9f4cdf62c29346a9c8644eb9cb376ee60119d15
michielstock <michielfmstock@gmail.com>
2020-12-02 09:20:29 +0100

    :tada: start softmax

---
+++ src/softmax.jl
@@ -0,0 +1,16 @@
+# Michiel Stock
+# Example of a source code file implementing a module.
+
+
+# all your code is part of the module you are implementing
+module Softmax
+
+# you have to import everything you need for your module to work
+# if you use a new package, don't forget to add it in the package manager
+
+# export all functions that are relevant for the user
+#export 
+
+
+
+end
