789449d0c3e9f06465c202193dc6d32ec748fcdb
f6v <i.am.filippov@gmail.com>
2020-12-03 13:48:48 +0100

    Function stub

--- src/single_cell_nmf.jl
+++ src/single_cell_nmf.jl
@@ -1,8 +1,19 @@
 module SingleCellNMF
 
-export foo
+export perform_nmf 
 
-function foo(a, b)
-	return a + b
+function perform_nmf(rna_data::Array{Float64}, atac_data::Array{Float64}, k::Int64)
+	if k <= 0
+		throw(ArgumentError("k should be positive"))
+	end
+	
+	n_rows_rna, n_cells = size(rna_data)
+	n_rows_atac, n_cells = size(atac_data)
+
+	H = zeros(k, n_cells)	
+	W_rna = zeros(n_rows_rna, k)
+	W_atac = zeros(n_rows_atac, k)
+
+	return H, W_rna, W_atac
 end
 end
--- test/single_cell_nmf.jl
+++ test/single_cell_nmf.jl
@@ -1,13 +1,23 @@
 @testset "SingleCellNMF" begin
 	using STMOZOO.SingleCellNMF
 	
-	a = 10
-	b = -3
+	@testset "arguments validation" begin
+		@test_throws ArgumentError perform_nmf(zeros(1, 1), zeros(1, 1,), -1)  
+	end
+	
+	@testset "returned values" begin
+		n_cells = 100
+		n_rows_rna = 500
+		n_rows_atac = 1000
+		rna_data = zeros(n_rows_rna, n_cells)
+		atac_data = zeros(n_rows_atac, n_cells)
+		k = 5
 
-	@testset "foo" begin
-		result = foo(a, b)
-		
-		@test result == 7
+		H, W_rna, W_atac = perform_nmf(rna_data, atac_data, k)
+
+		@test size(H) == (k, n_cells) 
+		@test size(W_rna) == (n_rows_rna, k)
+		@test size(W_atac) == (n_rows_atac, k)
 	end
 end
 
