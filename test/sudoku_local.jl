a7b272232a0eac104e60332a6639372e11b52232
francops1722 <57263330+francops1722@users.noreply.github.com>
2020-12-11 20:24:01 +0100

    test set added

---
+++ test/sudoku_local.jl
@@ -0,0 +1,21 @@
+@testset "Local_search" begin
+    using STMOZOO.Local_search
+
+    min_cost = Inf
+    solution =[]
+    for i in 1:1000
+        sol = search(sudoku_1, 1000, 1000)
+        if sudoku_cost(sol) == 0
+            solution = sol
+            break
+        end
+        if sudoku_cost(sol) < min_cost
+            min_cost = sudoku_cost(sol)
+            solution = sol
+        end
+        i += 1
+    end
+    #Test if a solution was founded or it is just an approximate solution
+    @test sudoku_cost(solution) == 0
+end
+    
