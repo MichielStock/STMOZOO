c024d46b426562a96cfc94b588fb8346531ab827
francops1722 <57263330+francops1722@users.noreply.github.com>
2020-12-11 18:53:19 +0100

    delete extra modules

--- src/STMOZOO.jl
+++ src/STMOZOO.jl
@@ -2,8 +2,7 @@ module STMOZOO
 
 
 # execute your source file and export the module you made
-include("example.jl")
 include("local_greedydesc.jl")
-export Example, Local_search, printSK, Sudokus  
+export Local_search  
 
 end # module
