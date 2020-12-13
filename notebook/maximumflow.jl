20f3582919415ee5c6e016980a17d947d58fa582
DouweDeVestele <72204409+DouweDeVestele@users.noreply.github.com>
2020-12-13 12:44:55 +0100

    initialized notebook

---
+++ notebook/maximumflow.jl
@@ -0,0 +1,21 @@
+### A Pluto.jl notebook ###
+# v0.12.15
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 814488a0-3d37-11eb-3dc9-a97e67207e93
+md"""
+# Maximum Flow Problem
+
+Exam project for the course Selected Topics on Mathematical Optimization
+
+author: Douwe De Vestele"""
+
+
+# ╔═╡ 91553c30-3d37-11eb-2a33-2d6d4ee5e9b2
+
+
+# ╔═╡ Cell order:
+# ╠═814488a0-3d37-11eb-3dc9-a97e67207e93
+# ╠═91553c30-3d37-11eb-2a33-2d6d4ee5e9b2
