69a0830ae88c35e8609f3c1915204f0fdc4122c6
csangara <sai.chananchida@gmail.com>
2020-12-16 21:08:54 +0100

    created documentation files

--- docs/make.jl
+++ docs/make.jl
@@ -5,9 +5,10 @@ using STMOZOO.Example
 
 makedocs(sitename="STMO ZOO",
     format = Documenter.HTML(),
-    modules=[Example], # add your module
+    modules=[Example, Raytracing], # add your module
     pages=Any[
         "Example"=> "man/example.md",  # add the page to your documentation
+        "Raytracing" => "man/raytracing.md"
     ])
 
 #=
---
+++ docs/src/man/raytracing.md
@@ -0,0 +1,15 @@
+## Raytracing
+
+```@docs
+create_scene
+get_circle
+get_circle_perimeter
+get_circle_inside
+dijkstra
+reconstruct_path
+get_neighbors
+plot_pixels
+plot_pixel_edges
+plot_paths
+plot_circle
+```
