9d965d0daf4ab3cdef627d179155144d29f747e8
DouweDeVestele <72204409+DouweDeVestele@users.noreply.github.com>
2020-12-16 12:06:05 +0100

    implemented bfs and finished augmenting_path

--- src/maximumflow.jl
+++ src/maximumflow.jl
@@ -1,40 +1,47 @@
-#Douwe De Vestele
+# Douwe De Vestele
 
 module MaximumFlow
 
 # add needed packages
 
 # export all functions
-export MFP,test_edge,test_mat ,edges,cap,res_network
-
-# struct MFP Array{Tuple{C::Number,V::Int,V::Int},1}
-#     capacity::C
-#     A::V
-#     B::V
-# end
+export cap,res_network,augmenting_path,bfs
 
 # Returns the edges of the graph
-edges(mf::Array{Tuple{Int,Int,Int},1}) = [mf[i][2:3] for i in 1:length(mf)] 
+# function edges(nw::Array{Int,2})
+#     es = []
+#     for i in nw
+#         i != 0 && push!(caps, i)
+#     end
+#     return caps
+# end
+# edges(mf::Array{Tuple{Int,Int,Int},1}) = [mf[i][2:3] for i in 1:length(mf)] 
 
-# Returns the capacities of the graph
-function cap(mf::Array{Int,2})
+"""
+    cap(nw::Array{Int,2})
+Returns the capacities of a graph `nw`implemented a an adjacency matrix.
+"""
+function cap(nw::Array{Int,2})
     caps = []
-    for i in mf
-        i !=0 && push!(caps,i)
+    for i in nw
+        i != 0 && push!(caps, i)
     end
     return caps
 end
-cap(mf::Array{Tuple{Int,Int,Int},1}) = [mf[i][1] for i in 1:length(mf)]
+# cap(nw::Array{Tuple{Int,Int,Int},1}) = [nw[i][1] for i in 1:length(nw)]
 
 """
     res_network(mf_oc::Array{Int,2},mf_cur::Array{Int,2})
-Calculates the residual network of a network implemented as an adjacency matrix.
+Calculates the residual network of a network from a maximum flow problem `mf_oc` 
+and a network containing the current flow `mf_cur` implemented as an adjacency matrix.
 """
-function res_network(mf_oc::Array{Int,2},mf_cur::Array{Int,2})
-    r = size(mf_oc,1) # rows
-    c = size(mf_oc,2) # columns
+function res_network(mf_oc::Array{Int,2}, mf_cur::Array{Int,2})
+    r = size(mf_oc, 1) # rows
+    c = size(mf_oc, 2) # columns
     @assert r == c "Adjacency matrix has to be square."
-    mf_res = zeros(Int,r,c)
+    @assert size(mf_cur, 1) == size(mf_cur, 2) "Adjacency matrix has to be square."
+    @assert r == size(mf_cur, 1) "Adjacency matrices need to have same size."
+    mf_res = zeros(Int, r, c)
     for i in 1:r
         for j in 1:c
             mf_res[i,j] = mf_oc[i,j] - mf_cur[i,j] + mf_cur[j,i]
@@ -43,37 +50,81 @@ function res_network(mf_oc::Array{Int,2},mf_cur::Array{Int,2})
     return mf_res
 end
 
-
-
+""" 
+    bfs(nw::Array{Int,2}, s::Int, t::Int)
+Searches an existing path in the network `nw` with a breadth-first-search from source `s` to sink `t`.
+Returns `true` if path exists and an array with the predecessor vertices to make backtracking possible. 
+Returns `false` if path does not exist.
 """
-    augmenting_path(mf::Array{Tuple{Int,Int,Int},1})
-Execute Ford-Fulkerson algorithm with DFS search to find maximum flow of a network G.
-"""
-function augmenting_path(mf::Array{Int,2})
-    @assert size(mf,1) == size(mf,2) "Adjacency matrix has to be square."
-    @assert all(mf .>=0) "There are negative flow capacities, which is not possible." 
-    max_flow = 0
-    #mf_edges = edges(mf)
-    mf_capacities = cap(mf)
-    # search existing paths
+    function bfs(nw::Array{Int,2}, s::Int, t::Int)
+    n = size(nw, 1) # number of vertices
+    @assert n == size(nw, 2) "Adjacency matrix has to be square."
+    @assert all(nw .>= 0) "There are negative flow capacities, which is not possible."
 
-    # add flow if possible
+    labeled = falses(n) # true if vertex is labeled
+    labeled[s] = true # start at source
+    L = [s] # labels, start at source
+    pred = zeros(Int, n) # predecessor labels
+
+    while ! labeled[t] && ! isempty(L)# as long as t is not labeled and there are still unvisited nodes
+        i = pop!(L) # select labeled node
+        for j in findall(nw[i,:] .> 0) # there is still possible flow from node i to node j
+        # if j is unlabeled, label it and save the predecessor
+            if ! labeled[j]
+                labeled[j] = true
+                pred[j] = i
+                push!(L, j)
+            end
+        end
+    end
+    return labeled[t], pred
 end
 
 """
-    e_list_to_adj_mat(elist::Array{Tuple{Int,Int,Int},1})
-Converts an edge list to an adjacency matrix
+    augmenting_path(mf::Array{Int,2}, s::Int, t::Int)
+Execute Ford-Fulkerson algorithm with breadth-first-search to find the maximum flow of a network from a maximum flow problem `mf` from source `s` to sink `t`.
 """
-function e_list_to_adj_mat(elist::Array{Tuple{Int,Int,Int},1})
+function augmenting_path(mf::Array{Int,2}, s::Int, t::Int)
+    n = size(mf, 1) # number of vertices
+    @assert n == size(mf, 2) "Adjacency matrix has to be square."
+    @assert all(mf .>= 0) "There are negative flow capacities, which is not possible." 
 
+    # initialization
+    max_flow = 0 # start with zero flow
+    cur_flow = zeros(Int, n, n) # start with zero flow
+    res_flow = res_network(mf, cur_flow)
+
+    # search for a path
+    @show path, pred = bfs(res_flow, s, t)
+
+    # if there is a path, add lowest capacity (=limiting) from the path to the flow.
+    while path
+    # backtracking
+        P = [t]
+        while P[end] != s
+            push!(P, pred[P[end]])
+        end
+        @show P
+    # calculate lowest capacity
+     @show   Δ = minimum([res_flow[x,y] for (y, x) in zip(P[1:end - 1], P[2:end]) ])
+    # add this flow to current flow
+        [cur_flow[x,y] += Δ for (y, x) in zip(P[1:end - 1], P[2:end])]
+        res_flow = res_network(mf, cur_flow) # update residual network
+    # search for a path
+    @show path, pred = bfs(res_flow, s, t)
+    end
+    max_flow = sum(cur_flow[:,t])
+    return cur_flow, max_flow
 end
 
-# test graph as edge list
-test_edge = [(8,0,1),(2,0,2),(4,1,3),(1,2,3)];
+# """
+#     e_list_to_adj_mat(elist::Array{Tuple{Int,Int,Int},1})
+# Converts an edge list to an adjacency matrix
+# """
+# function e_list_to_adj_mat(elist::Array{Tuple{Int,Int,Int},1})
 
-# test graph as adjacency matrix
-test_mat = [0 8 2 0;
-            0 0 2 4;
-            0 0 0 1;
-            0 0 0 0]
+# end
+
+# # test graph as edge list
+# test_edge = [(8, 0, 1),(2, 0, 2),(4, 1, 3),(1, 2, 3)];
 end
