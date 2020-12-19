0e81a521c64ae9e6ee189cf17ea3c5a5ba609890
Ke <ke210zhang@gmail.com>
2020-12-19 21:27:16 +0100

    Update STMOZOO.jl

--- src/STMOZOO.jl
+++ src/STMOZOO.jl
@@ -2,8 +2,8 @@ module STMOZOO
 
 
 # execute your source file and export the module you made
-#include("example.jl")
-#export Example
+include("example.jl")
+export Example
 
 include("EulerianPath.jl")
 export Eulerian_path
