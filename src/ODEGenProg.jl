9f1c3a17dc2203636c43c907d9de218f863ccb38
MMichaelVdV <mmichaelvandevoorde@gmail.com>
2020-12-22 19:48:44 +0100

    add basic test function

--- src/ODEGenProg.jl
+++ src/ODEGenProg.jl
@@ -7,12 +7,44 @@ module ODEGenProg
 
 # you have to import everything you need for your module to work
 # if you use a new package, don't forget to add it in the package manager
+using ExprRules, ExprOptimization, Random, Plots, Calculus
+
 
 # export all functions that are relevant for the user
-export foo_bar
+export foo_bar, fizzywop_test
 
+"""
+ this is a testfunction
+"""
 function foo_bar(x::Int64,y::Int64)
     return x+y
+end
+
+"""
+    fizzywop_test(tree::RuleNode, grammar::Grammar)
+This is a hardcoded fitness function to solve the differential equation f'(x) - f(x) = 0, 
+with boundary condition f(0) = 1. The expected solution is f(x) = exp(x).
+
+"""
+function fizzywop_test(tree::RuleNode, grammar::Grammar)
+    ex = get_executable(tree, grammar)
+    score = 0.0
+	#domain
+    for x = 0.0:0.1:1.0
+		S[:x] = x
+		score += try (abs(Core.eval(S,differentiate(ex)) - Core.eval(S,ex)))^2
+		catch
+			return Inf
+		end
+    end
+	#boundary conditions
+	S[:x] = 0
+	λ = 100.
+	score += try λ*(((Core.eval(S,ex)-1))^2)
+	catch
+		return Inf
+	end
+	return score
 end
 
 end
--- test/odegenprog.jl
+++ test/odegenprog.jl
@@ -1,14 +1,31 @@
 @testset "ODEGenProg" begin
 
     using STMOZOO.ODEGenProg
-
     x = 4
     y = 4
-
     s = foo_bar(x,y)
-
     @test s == 8
 
+    grammar = @grammar begin
+        R = |(1:9)
+        R = R + R
+        R = R - R
+        R = R / R
+        R = R * R
+        R = x ^ R
+        R = sin(R)
+        R = cos(R)
+        R = exp(R)
+        R = log(R)
+        R = x
+        #R = y
+        #R = z
+    end
+    
+    g = GeneticProgram(250,50,5,0.3,0.3,0.4)
+    results_gp = optimize(g, grammar, :R, fizzywop_test)
+
+    @test results_gp.expr == :(exp(x))
 
 
 end
