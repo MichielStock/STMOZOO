861749b87f3dc7fcfb5840181d328fe9fbe5d889
MMichaelVdV <mmichaelvandevoorde@gmail.com>
2020-12-23 00:17:31 +0100

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -5,9 +5,10 @@ using STMOZOO.Example
 
 makedocs(sitename="STMO ZOO",
     format = Documenter.HTML(),
-    modules=[Example], # add your module
+    modules=[Example, ODEGenProg], # add your module
     pages=Any[
-        "Example"=> "man/example.md",  # add the page to your documentation
+        "Example" => "man/example.md",  # add the page to your documentation
+        "ODEGenProg" => "man/odegenprog.md",
     ])
 
 #=
