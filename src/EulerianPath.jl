2eeab84cb067c69a2e6603dbbcff3188a4e0d589
Ke <ke210zhang@gmail.com>
2020-12-22 19:30:58 +0100

    Documentation

--- src/EulerianPath.jl
+++ src/EulerianPath.jl
@@ -1,10 +1,33 @@
 module EulerianPath
 
-export  test1 
-
-function test1(x)
-    x = 1
-    return x
+export  create_adj_list
+using DataStructures
+"""
+This is function to create adjacent list.
+The input is a list of edges, the output is a dictionary. 
+The key is each node, the value is the node has edges with it.
+"""
+function create_adj_list(edges)
+    adj_list = DefaultDict{Int, Array{Int}}(() -> Vector{Int}())
+        for (x, y) in edges
+        push!(adj_list[x], y)
+        push!(adj_list[y], x)
+        end
+return adj_list 
 end
+
+function has_eulerian_cycle(adj_list)
+    odd = 0
+    for node in adj_list
+        if len(adj_list[node]) % 2 != 0
+            odd += 1
+        end
+    end
+    if odd == (0 || 2)
+        return True
+    end
+    return False
+end
+
 
 end
--- test/EulerianPath.jl
+++ test/EulerianPath.jl
@@ -1,12 +1,9 @@
-
 @testset "EulerianPath" begin
     using STMOZOO.EulerianPath
-    @testset "test1" begin
 
-    @test test1(1) == 1
+    @testset "create_adj_list" begin
+        
+    @test create_adj_list([[1, 5], [1, 2], [1, 4], [1, 3], [5, 2], [2, 3], [2, 4], [3, 4], [3, 6], [4, 6]]) == {1: [5, 2, 4, 3], 5: [1, 2], 2: [1, 5, 3, 4], 4: [1, 2, 3, 6], 3: [1, 2, 4, 6], 6: [3, 4]}
 
-    @test test1(2) == 1
-    
     end
-    
 end
