0a04f3265f20ab10d120129a967ea286293611ae
michielstock <michielfmstock@gmail.com>
2020-12-23 16:20:29 +0100

    :wrench: suggestions

--- src/EulerianPath.jl
+++ src/EulerianPath.jl
@@ -1,7 +1,11 @@
 module EulerianPath
 
 export  create_adj_list, has_eulerian_cycle
+
+
 """
+    create_adj_list(edges)
+
 This is function to create adjacent list.
 The input is a list of edges, the output is a dictionary. 
 The key is each node, the value is the node has edges with it.
@@ -25,6 +29,8 @@ return adj_list
 end
 
 """
+    has_eulerian_cycle(adj_list)
+
 This is the function to test if the dictionary of nodes, 
 whose key is for each node and the value is the list of nodes that has edges,
 has an eulerian cycle. 
@@ -32,12 +38,11 @@ has an eulerian cycle.
 It is based on the theory :
 An undirected graph has an Eulerian trail if and only if exactly zero or two vertices have odd degree, 
 and all of its vertices with nonzero degree belong to a single connected component.
-
 """
 
 function has_eulerian_cycle(adj_list)
     odd = 0
-    for node in keys(adj_list)
+    for node in keys(adj_list)  #FIXME: there are functions `isodd` and `iseven` you can use, in attition to `count` etc
         if length(adj_list[node]) % 2 != 0
             odd += 1
         end
