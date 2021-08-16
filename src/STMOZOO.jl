b2e6497bc01b87551a3923500e4cdac8b9902fb1
michielstock <michielfmstock@gmail.com>
2021-08-16 09:17:50 +0200

    🚑 fix conflicts

--- src/STMOZOO.jl
+++ src/STMOZOO.jl
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 module STMOZOO
 
 
@@ -7,13 +7,4 @@ include("sudoku.jl")
 export SudokuSolver
 
 end # module
-=======
-module STMOZOO
 
-
-# execute your source file and export the module you made
-include("sudoku.jl")
-export SudokuSolver
-
-end # module
->>>>>>> 559f20d740a7b6a5a0bc748eff7ecf6cb6ce720e
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 using Test
 
 include("sudokutest.jl")
-=======
-using Test
-
-include("sudokutest.jl")
->>>>>>> 559f20d740a7b6a5a0bc748eff7ecf6cb6ce720e
-# add here the file with your unit tests
