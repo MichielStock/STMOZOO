89cbc52b4344048c32ec13b0394ec7e509db743f
Jietse <jietse.verweirder@ugent.be>
2021-11-30 17:35:31 +0100

    Initial outline of notebook

---
+++ notebook/geneticalgorithms.jl
@@ -0,0 +1,56 @@
+### A Pluto.jl notebook ###
+# v0.17.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 171fee18-20f6-11eb-37e5-2d04caea8c35
+md"""
+# Genetic Algorithms
+
+By Jordi Verbruggen & Jietse Verweirder
+
+## Introduction
+
+Explain what genetic algorithms are and the relationship with real evolution (mutation, selection, ...)
+
+"""
+
+# ╔═╡ b95fd05e-f139-47f4-ab69-79f5dd3cc279
+md"""
+## Initial population
+"""
+
+# ╔═╡ 37b9fce9-e99c-424f-a614-8169b7edf0d6
+md"""
+## Fitness
+"""
+
+# ╔═╡ ccb197ff-f322-4f14-acb5-ba64a343d234
+md"""
+## Termination criteria
+"""
+
+# ╔═╡ c6155621-9a86-4747-b0ec-65e779670b57
+md"""
+## Selection
+"""
+
+# ╔═╡ 35ef4b6a-5ff7-4ca0-be54-9b281dfe6347
+md"""
+## Crossover
+"""
+
+# ╔═╡ 3a075aab-dbf8-4120-93bc-337991c65b40
+md"""
+## Mutation
+"""
+
+# ╔═╡ Cell order:
+# ╠═171fee18-20f6-11eb-37e5-2d04caea8c35
+# ╠═b95fd05e-f139-47f4-ab69-79f5dd3cc279
+# ╠═37b9fce9-e99c-424f-a614-8169b7edf0d6
+# ╠═ccb197ff-f322-4f14-acb5-ba64a343d234
+# ╠═c6155621-9a86-4747-b0ec-65e779670b57
+# ╠═35ef4b6a-5ff7-4ca0-be54-9b281dfe6347
+# ╠═3a075aab-dbf8-4120-93bc-337991c65b40
