7fa9355c722d9b04b0d655335038741795c66b07
Jietse <jietse.verweirder@ugent.be>
2021-12-29 14:30:44 +0100

    Dividing the work

--- notebook/geneticalgorithms.jl
+++ notebook/geneticalgorithms.jl
@@ -12,6 +12,8 @@ By Jordi Verbruggen & Jietse Verweirder
 
 ## Introduction
 
+Jietse
+
 Explain what genetic algorithms are and the relationship with real evolution (mutation, selection, ...)
 
 """
@@ -19,31 +21,50 @@ Explain what genetic algorithms are and the relationship with real evolution (mu
 # ╔═╡ b95fd05e-f139-47f4-ab69-79f5dd3cc279
 md"""
 ## Initial population
+Jordi
 """
 
 # ╔═╡ 37b9fce9-e99c-424f-a614-8169b7edf0d6
 md"""
 ## Fitness
+Jordi
 """
 
 # ╔═╡ ccb197ff-f322-4f14-acb5-ba64a343d234
 md"""
 ## Termination criteria
+
+Jordi 
 """
 
 # ╔═╡ c6155621-9a86-4747-b0ec-65e779670b57
 md"""
 ## Selection
+
+Jietse
 """
 
 # ╔═╡ 35ef4b6a-5ff7-4ca0-be54-9b281dfe6347
 md"""
 ## Crossover
+
+Jordi
 """
 
 # ╔═╡ 3a075aab-dbf8-4120-93bc-337991c65b40
 md"""
 ## Mutation
+
+Jietse
+"""
+
+# ╔═╡ 9445dfa1-c276-43fd-81cc-752645e8bf43
+md"""
+## Implementation
+
+samen
+
+graph coloring with genetic algorithm
 """
 
 # ╔═╡ Cell order:
@@ -54,3 +75,4 @@ md"""
 # ╠═c6155621-9a86-4747-b0ec-65e779670b57
 # ╠═35ef4b6a-5ff7-4ca0-be54-9b281dfe6347
 # ╠═3a075aab-dbf8-4120-93bc-337991c65b40
+# ╠═9445dfa1-c276-43fd-81cc-752645e8bf43
