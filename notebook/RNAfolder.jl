b0f5c0e719adbed84b7bd2b63397d1dc349cc10f
menvdamm <80266975+menvdamm@users.noreply.github.com>
2021-12-31 12:51:29 +0100

    Create RNAfolder.jl

---
+++ notebook/RNAfolder.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.17.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 4b3e86ea-6973-11ec-014f-9f2708ba86cf
+
+
+# ╔═╡ Cell order:
+# ╠═4b3e86ea-6973-11ec-014f-9f2708ba86cf
