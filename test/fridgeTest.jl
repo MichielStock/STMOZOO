b13e0435b29af6d77990b0025d8b85ee318d7fde
warvbell <wardvanbelle@gmail.com>
2022-01-30 21:33:07 +0100

    added tests for RandomCombo

--- test/fridgeTest.jl
+++ test/fridgeTest.jl
@@ -11,21 +11,16 @@
     testDict2 = Dict("fries" => ["potato","salt"], 
         "macaroni" => ["cheese", "salt", "jambon"])
 
+    testDict3 = Dict("fries" => ["potato","salt"], 
+        "macaroni" => ["cheese", "salt", "jambon"], 
+        "salad" => ["cabbage","tomato","oil","horseradish"])
+
     @testset "checkIngredients" begin
         # check if all ingredients are passed if no replacement is needed
         @test checkIngredients(testList,["cheese","salt","tomato","chocolate"]) == testList 
 
         # test for type
         @test checkIngredients(testList,["cheese","salt","tomato","chocolate"]) isa Vector{String}
-    end
-
-    @testset "fridgeObjective" begin
-        # test objective for one recipe
-        @test fridgeObjective([0,1,0,1,2]) == 16
-
-        # test objective for multiple recipes
-        @test fridgeObjective([[0,1,0,1,2],[1,0,0,0,3]]) == 40
-        @test fridgeObjective([[0,1,0,1,2],[1,1,0,0,3]]) == Inf
     end
 
     @testset "greedyFindCombo" begin
@@ -39,4 +34,14 @@
         @test greedyFindCombo(testList, testDict2, 3) == Dict("macaroni" => [1,0,0,0,2], "fries" => [0,1,0,0,1])
     end
 
+    @testset "RandomCombo" begin
+        # test all ingredients used end
+        @test RandomCombo(testList, testDict3, 3) == Dict("macaroni" => [1,0,0,0,2], "salad" => [0,0,1,1,2], "fries" => [0,1,0,0,1])
+
+        # test no recipes left end
+        @test RandomCombo(testList, testDict2, 3) == Dict("macaroni" => [1,0,0,0,2], "fries" => [0,1,0,0,1])
+
+        # test length of max number recipes end
+        @test length(values(RandomCombo(testList, testDict, 2))) == 2
+    end
 end
