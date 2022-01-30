7a9bcf9200c6d5954c5f44f2a6808972a0561abc
Peter <ppm1337@outlook.com>
2022-01-30 21:34:14 +0100

    allow run_experiments save subplots

--- src/experiments.jl
+++ src/experiments.jl
@@ -1,38 +1,57 @@
 module Experiments
 
-using PlotlyJS
-
 include("data.jl")
 include("neuralnetwork.jl")
 include("plotlib.jl")
+include("plotutils.jl")
 include("structs.jl")
 
-function run_experiments(experiments; prefix = "", show_plots = true, save_plots = true, plot_size = "medium")
-    plot_width, plot_height = get_plot_resolution(plot_size)
+using PlotlyJS
+
+export run_experiments
+
+plot_dir = "plots"
+
+function run_experiments(experiments; prefix = "", random_moons = true, show_plots = true, save_plots = true, plot_size = "medium", plot_type = "both", args...)
+	plot_type = lowercase(plot_type)
+	@assert plot_type in ["boundary", "stats", "both"] "Requested plot type '$plot_type' is not supported."
+
+    plot_width, plot_height = PlotUtils.get_plot_resolution(plot_size)
 
     for (name, def) in experiments
         # load data
-        train_loader = Data.get_moon_data_loader(offset = def.offset)
+		if random_moons
+        	train_loader = Data.get_moon_data_loader(offset = def.offset)
+		else
+			train_loader = Data.get_moon_data_loader(offset = def.offset, seed = 1337)
+		end
 
         # create and train the model
-        model, stats = NeuralNetwork.train(train_loader, def.optimizer, def.sd)
+        model, stats = NeuralNetwork.train(train_loader, def.optimizer, def.sd; args...)
 
         # create plots
         p1 = PlotLib.plot_decision_boundary(train_loader, model, title = name)
         p2 = PlotLib.plot_loss_and_accuracy(stats["loss"], stats["accuracy"])
-		# combine subplots
-        p = [p1 p2]
-		# make background and plots transparent (looks better in dark-styled browsers)
-        PlotlyJS.relayout!(p, paper_bgcolor = "rgba(0, 0, 0, 0)", plot_bgcolor = "rgba(0, 0, 0, 0)")
+
+		if plot_type == "both"
+			# combine subplots
+			p = [p1 p2]
+			# make background and plots transparent (looks better in dark-styled browsers)
+			PlotlyJS.relayout!(p, paper_bgcolor = "rgba(0, 0, 0, 0)", plot_bgcolor = "rgba(0, 0, 0, 0)")
+		elseif plot_type == "boundary"
+			p = p1
+		elseif plot_type == "stats"
+			p = p2
+		end
         
-		# save and/or display decision boundary and performance stats
+		# save requested plot
         if save_plots 
-			p_name = "plots/$(prefix)" * replace(name, " " => "_") * ".png"
+			p_name = "$plot_dir/$prefix" * (prefix != "" ? "_" : "") * replace(name, " " => "_") * (plot_type == "both" ? "" : plot_type) * ".png"
 			PlotlyJS.savefig(p, p_name, width = plot_width, height = plot_height) 
 		end
 
         if show_plots 
-			display(p) 
+			display(p)
 		end
     end
 	@info "Finished $(length(experiments)) experiments."
